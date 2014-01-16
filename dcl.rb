249c9b37cd1adf1faf4688ae1ce580dde2dc40a3
Youhei SASAKI <uwabami@gfd-dennou.org>
2014-01-16 19:53:02 +0900

    Add dcl
    Signed-off-by: Youhei SASAKI <uwabami@gfd-dennou.org>

---
+++ dcl.rb
@@ -0,0 +1,26 @@
+require "formula"
+
+# Documentation: https://github.com/Homebrew/homebrew/wiki/Formula-Cookbook
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Dcl < Formula
+  homepage "http://www.gfd-dennou.org/library/dcl/"
+  url "http://www.gfd-dennou.org/library/dcl/dcl-5.4.8.tar.gz"
+  sha1 "58aa1bd1226dbbb49e4d725cccdcac211cde94eb"
+
+  depends_on :x11, :gfortran
+
+  def install
+    ENV.deparallelize
+
+    system "./configure", "--enable-shared",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make install"
+  end
+
+  test do
+    system "false"
+  end
+end
