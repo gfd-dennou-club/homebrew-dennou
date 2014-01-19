951c277db8ea3868ee1e45babbae85023d8d5b0a
Youhei SASAKI <uwabami@gfd-dennou.org>
2014-01-19 20:04:47 +0900

    Add dcl-c
    Signed-off-by: Youhei SASAKI <uwabami@gfd-dennou.org>

---
+++ dcl-c.rb
@@ -0,0 +1,39 @@
+require "formula"
+
+# Documentation: https://github.com/Homebrew/homebrew/wiki/Formula-Cookbook
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class DclC < Formula
+  homepage ""
+  url "http://www.gfd-dennou.org/library/dcl/dcl-5.4.8-C.tar.gz"
+  sha1 "98429d55d8eca835d2600d52da5f3dc45632cb13"
+  version "5.4.8"
+
+  depends_on :x11
+  depends_on :gtk2
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test dcl-c`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
