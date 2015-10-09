ff0aec0910ba316e846de02c7bdd6628039d5eb7
Youhei SASAKI <uwabami@gfd-dennou.org>
2015-10-09 18:19:31 +0900

    Update dclc: 6.0.2
    Signed-off-by: Youhei SASAKI <uwabami@gfd-dennou.org>

--- dclc.rb
+++ dclc.rb
@@ -1,15 +1,10 @@
 require "formula"
 
-# Documentation: https://github.com/Homebrew/homebrew/wiki/Formula-Cookbook
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Dclc < Formula
   homepage ""
-  url "http://www.gfd-dennou.org/arch/dcl/dcl-5.9.0-C.tar.gz"
-  sha1 "75c2359d7aaa472365af46401abd031445646ffe"
-  version '5.9.0'
-
+  url "https://www.gfd-dennou.org/library/dcl/dcl-6.0.2-C.tar.gz"
+  sha1 "d4446ca51cd08eb19fcbfeb083dc7deae41b5ec4"
+  version '6.0.2'
 
   depends_on 'pkg-config' => :build
   depends_on :x11
@@ -18,9 +13,6 @@ class Dclc < Formula
   depends_on 'atk'
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Remove unrecognized options if warned by configure
     system "./configure", "--prefix=#{prefix}",
                           "--x-includes=/opt/X11/include",
                           "--x-libraries=/opt/X11/lib"
