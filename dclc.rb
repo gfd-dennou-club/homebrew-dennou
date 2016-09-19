fe24d472ec6a316b1c2dadc35ac86ae17cae9610
Youhei SASAKI <uwabami@gfd-dennou.org>
2016-09-19 17:31:54 +0900

    Fix sha256sum
    Signed-off-by: Youhei SASAKI <uwabami@gfd-dennou.org>

--- dclc.rb
+++ dclc.rb
@@ -1,9 +1,9 @@
-class DclC < Formula
+class Dclc < Formula
   desc ""
   homepage ""
   url "https://www.gfd-dennou.org/library/dcl/dcl-7.1.1-C.tar.gz"
   version "7.1.1"
-  sha256 "57d8924d0c1245af239dee6abc1bbf82926dd86f1dd7ac3911b216cfc6a30e13"
+  sha256 "5d7e684c0de5d70730a549da6173112ad6d9c0ffffd958ce313685858f59dc1d"
 
   depends_on 'gtk+'
 
