254c7042d825a7ad4e69eb70867ad61c81bf4827
Youhei SASAKI <uwabami@gfd-dennou.org>
2016-09-19 16:51:18 +0900

    Version bump: dcl
    Signed-off-by: Youhei SASAKI <uwabami@camellia.kusm-uwabami.net>

--- dcl.rb
+++ dcl.rb
@@ -1,30 +1,21 @@
-require "formula"
-
 class Dcl < Formula
-  homepage "http://www.gfd-dennou.org/library/dcl/"
-  url "http://www.gfd-dennou.org/library/dcl/dcl-6.0.2.tar.gz"
-  sha1 "2e7e27da33cbe2d6bcb76ec5550f4c6dc20a1c95"
-  version '6.0.2'
+  desc ""
+  homepage ""
+  url "https://www.gfd-dennou.org/library/dcl/dcl-7.1.1.tar.gz"
+  version "7.1.1"
+  sha256 "57d8924d0c1245af239dee6abc1bbf82926dd86f1dd7ac3911b216cfc6a30e13"
 
-  # gfortran, enable OpenMP
-  depends_on :fortran => "without-multilib"
-  # for gtk support
-  depends_on :x11
-  depends_on 'pkg-config'
-  depends_on 'glib'
-  depends_on 'gtk+'
-  depends_on 'atk'
+  depends_on :fortran
+  depends_on :'gtk+'
 
   def install
     ENV.deparallelize
-    system "./configure", "--prefix=#{prefix}",
-                          "--x-includes=/opt/X11/include",
-                          "--x-libraries=/opt/X11/lib"
+    system "./configure", "--prefix=#{prefix}"
     system "make"
-    system "make install"
+    system "install"
   end
 
   test do
-    system "false"
+    system "true"
   end
 end
