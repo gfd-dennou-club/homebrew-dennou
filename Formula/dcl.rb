fe096bac9c73065955f07c388c66ad50f6cb9711
Youhei SASAKI <uwabami@gfd-dennou.org>
2025-07-10 13:31:51 +0900

    Upate formula
    Signed-off-by: Youhei SASAKI <uwabami@gfd-dennou.org>

--- Formula/dcl.rb
+++ Formula/dcl.rb
@@ -1,17 +1,20 @@
 class Dcl < Formula
   desc "This is a Fortran version of DCL, a drawing library useful for planetary and Earth sciences. This library includes graphical, character processing, and mathematical libraries."
   homepage ""
-  url "http://www.gfd-dennou.org/library/dcl/dcl-7.3.4.tar.gz"
-  version "7.3.4"
-  sha256 "5f38d8206f3f473321dca3717bc060921c5360ed5d47403b50677c847c439f93"
+  url "https://dennou-k.gfd-dennou.org/library/dcl/dcl-7.5.2.tar.gz"
+  version "7.5.2"
+  sha256 "8cb9979064c0acf244a27035a7e2e40a555f20ff1cd3949a248138598ad90c58"
+  license "BSD-2-Clause"
 
   depends_on 'gcc'
-  depends_on 'gtk+'
+  depends_on 'gtk+3'
   depends_on 'pkg-config'
 
   def install
     ENV.deparallelize
-    system "./configure", "--prefix=#{prefix}", "--with-gtk2"
+    ENV["FCFLAGS"] = "-g -O2 -fallow-invalid-boz -std=legacy"
+    ENV["FFLAGS"] = ENV["FCFLAGS"]
+    system "./configure", "--prefix=#{prefix}", "--with-gtk3"
     system "make"
     system "make install"
   end
